a339685233139534a7dabeabb47477158c6de05c
hoangdv2429 <hoangdv2429@gmail.com>
2021-11-10 01:10:07 +0700

    update till week 5

---
+++ lab05/Webpage.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>Lab05 Exercise </title>
+</head>
+
+<body>
+    <form action="Handle.php" method="POST">
+        <label>Title: </label>
+        <input type="text" name="title" value="">
+        <br>
+        <label>Year: </label>
+        <input type="text" name="year" value="">
+        <br>
+        <label>Copyright: </label>
+        <input type="text" name="copyright" value="">
+        <br>
+        <label>Content: </label>
+        <textarea name="content" rows="8" cols="80"></textarea>
+        <br>
+        <input type="submit" name="" value="SUBMIT">
+    </form>
+</body>
+
+</html>
